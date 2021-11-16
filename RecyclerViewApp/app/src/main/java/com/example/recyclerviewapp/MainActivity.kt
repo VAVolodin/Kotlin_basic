94678c3848e618c8848340cb4890204697a67254
Vladimir00505 <vladimir00505@gmail.com>
2021-11-16 21:00:33 +0300

    creating studying RecyclerView

---
+++ RecyclerViewApp/app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.recyclerviewapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val userNameList: List<String> = listOf("Абрам","Аваз","Август","Авдей","Автандил","Адам","Адис","Адольф","Адриан","Азарий","Аким","Алан","Ален","Александр","Алексей","Альберт","Альфред","Амадей","Амадеус","Амаяк","Анатолий","Ангел","Андоим","Андрей","Аникита","Антон","Ануфрий","Онуфрий","Арам","Арий","Аристарх","Аркадий","Арно","Арнольд","Арон","Арсен, ","Арсений","Артем")
+
+        val userRecyclerView: RecyclerView = findViewById(R.id.users_recyclerView)
+        userRecyclerView.layoutManager =
+            LinearLayoutManager(this,LinearLayoutManager.VERTICAL, false)
+        userRecyclerView.adapter = UserAdapter(userNameList)
+    }
+
+
+    }
+
---
+++ RecyclerViewApp/app/src/main/java/com/example/recyclerviewapp/UserViewHolder.kt
@@ -0,0 +1,16 @@
+package com.example.recyclerviewapp
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(userName:String){
+        val userNameTextView:TextView = itemView.findViewById(R.id.user_name_text_view)
+        userNameTextView.text = userName
+
+    }
+
+
+}
